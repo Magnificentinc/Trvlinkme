4d6c1e09d3f794c54dd884ce995c286c52b0b055
Arie <arie@wego.com>
2013-10-27 22:15:04 +0700

    Initial Commit

---
+++ Netra/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Netra
+//
+//  Created by Arie on 10/27/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NetraAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([NetraAppDelegate class]));
+	}
+}
